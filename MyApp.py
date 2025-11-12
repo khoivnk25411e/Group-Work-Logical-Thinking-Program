8d14f1d723406c5966066925db20ee7c8453c7c9
khoivnk25411e <khoivnk25411e@st.uel.edu.vn>
2025-11-12 23:52:37 +0700

    fix

--- MyApp.py
+++ MyApp.py
@@ -1,7 +1,9 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow
 
+from MainWindowEx import MainWindowEx
+
 app=QApplication([])
 myWindow= MainWindowEx()
 myWindow.setupUi(QMainWindow())
-myWindow.show()
+myWindow.ShowWindow()
 app.exec()
