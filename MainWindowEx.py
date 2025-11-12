16901b4786f459ec3f5e67038324fca9aca3775b
khoivnk25411e <khoivnk25411e@st.uel.edu.vn>
2025-11-12 23:29:10 +0700

    MainWindowEx

--- MainWindowEx.py
+++ MainWindowEx.py
@@ -1,1 +1,25 @@
+<<<<<<< Updated upstream
 class MainWindowEx(Ui_MainWindow):
+=======
+from MainWindow import Ui_MainWindow
+
+
+class MainWindowEx(Ui_MainWindow):
+    def setupUi(self, MainWindow):
+        super().setupUi(MainWindow)
+        self.setupSignalandSlots()
+    def setupSignalandSlots(self):
+        self.pushButtonClose.clicked.connect(self.process_close)
+        self.pushButtonClear.clicked.connect(self.process_clear)
+    def ShowWindow(self):
+        self.MainWindow.show()
+    def process_close(self):
+        self.MainWindow.close()
+    def process_clear(self):
+        self.lineEditA.clear()
+        self.lineEditB.clear()
+        self.lineEditC.clear()
+        self.lineEditResult.clear()
+        self.lineEditA.setFocus()
+    
+>>>>>>> Stashed changes
